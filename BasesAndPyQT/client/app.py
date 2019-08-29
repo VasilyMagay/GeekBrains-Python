8cf7dd05219307da1e83026891a25005a2c4f103
Vasily <vasily.magay@mail.ru>
2019-08-29 08:10:07 +0300

    dz

--- BasesAndPyQT/client/app.py
+++ BasesAndPyQT/client/app.py
@@ -6,19 +6,18 @@ import json
 from socket import socket
 from datetime import datetime
 
-
-class ClientVerifier(type):
-    def __new__(cls, clsname, bases, clsdict):
-        return type.__new__(cls, clsname, bases, clsdict)
-
-    def __init__(self, clsname, bases, clsdict):
-        # for key, value in clsdict.items():
-        #     print(f'{key}={value}')
-        type.__init__(self, clsname, bases, clsdict)
+from PyQt5.QtWidgets import (
+    QMainWindow, QPushButton, QLabel, QTextEdit,
+    QPlainTextEdit, QWidget, QVBoxLayout
+)
+from PyQt5 import QtCore
 
 
-class Client(metaclass=ClientVerifier):
+class Client(QMainWindow):
     def __init__(self, host, port, buffersize):
+        super().__init__()
+        self.init_ui()
+
         self._host = host
         self._port = port
         self._buffersize = buffersize
@@ -46,7 +45,9 @@ class Client(metaclass=ClientVerifier):
                 pass
             else:
                 b_response = zlib.decompress(compressed_response)
-                logging.info(b_response.decode())
+                s_response = b_response.decode()
+                logging.info(s_response)
+                self.message_list.setPlainText(s_response)
 
     def send(self, action, data):
         hash_obj = hashlib.sha256()
@@ -73,3 +74,42 @@ class Client(metaclass=ClientVerifier):
 
     def stop(self):
         self._sock.close()
+
+    def init_ui(self):
+
+        self.send_button = QPushButton("Send Message", self)
+        self.send_button.clicked.connect(self.click_send_button)
+
+        self.message_label = QLabel("Message:")
+
+        self.message_text = QTextEdit()
+        self.message_text.setGeometry(QtCore.QRect(10, 10, 370, 50))
+
+        self.message_list_label = QLabel("Protocol:")
+
+        self.message_list = QPlainTextEdit()
+        self.message_list.setGeometry(QtCore.QRect(10, 10, 370, 200))
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.message_list_label)
+        main_layout.addWidget(self.message_list)
+        main_layout.addWidget(self.message_label)
+        main_layout.addWidget(self.message_text)
+        main_layout.addWidget(self.send_button)
+        # main_layout = QGridLayout()
+        # main_layout.addWidget(self.message_label, 0, 0, QtCore.Qt.AlignTop)
+        # main_layout.addWidget(self.message_text, 0, 1)
+        # main_layout.addWidget(self.send_button, 1, 0)
+
+
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+
+        self.setCentralWidget(central_widget)
+
+        # Установка заголовка и размеров главного окна
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Messenger')
+
+    def click_send_button(self):
+        self.send('echo', self.message_text.toPlainText())
