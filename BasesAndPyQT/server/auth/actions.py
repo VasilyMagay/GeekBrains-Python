330e06c5fdf04d566c4754d217929ae024c9f17c
Vasily <vasily.magay@mail.ru>
2019-09-02 13:53:02 +0300

    dz

--- BasesAndPyQT/server/auth/actions.py
+++ BasesAndPyQT/server/auth/actions.py
@@ -1,9 +1,13 @@
 from .controllers import (
-    get_users_controller, add_user_controller, del_user_controller
+    get_users_controller, add_user_controller, del_user_controller,
+    login_controller, registration_controller, logout_controller
 )
 
 actionnames = [
     {'action': 'get_users', 'controller': get_users_controller},
     {'action': 'add_user', 'controller': add_user_controller},
     {'action': 'del_user', 'controller': del_user_controller},
+    {'action': 'login', 'controller': login_controller},
+    {'action': 'registrate', 'controller': registration_controller},
+    {'action': 'logout', 'controller': logout_controller},
 ]
--- BasesAndPyQT/server/decorators.py
+++ BasesAndPyQT/server/decorators.py
@@ -3,7 +3,6 @@ import sys
 
 from functools import wraps
 from datetime import datetime
-from protocol import make_response
 
 
 logger = logging.getLogger('decorators')
@@ -19,12 +18,3 @@ def logged(func):
 
 def func_name():
     return sys._getframe(1).f_code.co_name
-
-
-def login_required(func):
-    @wraps(func)
-    def wrapper(request, *args, **kwargs):
-        if 'token' not in request:
-            return make_response(request, 403, 'Access denied')
-        return func(request, *args, **kwargs)
-    return wrapper
